6a48cd48df6227e50317905d016cd9ff8434a72c
Wesley Beck <wes.beck21@outlook.com>
2022-06-08 21:14:06 -0600

    comment stretch features

--- services/TwitterFeed.js
+++ services/TwitterFeed.js
@@ -1,4 +1,4 @@
-// TODO
+// STRETCH FEATURE
 /*
   Build container element to house data for twitter feed for valheim creators' updates
   Build a function which fetches data to render to the DOM element
