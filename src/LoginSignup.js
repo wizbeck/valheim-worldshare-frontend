cc10e7d86a101478c3f74b9429167aa5a777420b
Wesley Beck <wes.beck21@outlook.com>
2022-06-10 22:01:37 -0600

    Login form renders properly, need brs

--- src/LoginSignup.js
+++ src/LoginSignup.js
@@ -1,6 +1,6 @@
 // Utility class for building Login Form
 class LoginForm {
-  constructor(id) {
+  constructor(id="LoginForm") {
     this.rootNode = document.createElement("FORM");
     this.email = document.createElement("INPUT");
     this.password = document.createElement("INPUT");
@@ -15,7 +15,7 @@ class LoginForm {
       id: 'EmailInput',
       className: 'LoginInput',
       required: true
-    })
+    });
     labelFor(this.email, 'EmailInput')
     this.rootNode.appendChild(this.email)
 
@@ -24,20 +24,19 @@ class LoginForm {
       id: 'PasswordInput',
       className: 'LoginInput',
       required: true
-    })
+    });
     labelFor(this.password, 'PasswordInput')
     this.rootNode.appendChild(this.password)
 
     this.addElAttrs(this.submit, {
-      type: 'submit',
-      value: 'Sign In',
-    })
+      id: 'LoginBtn'
+      type: 'submit'
+    });
+    this.submit.innerText = 'Sign In'
     this.rootNode.appendChild(this.submit)
 
-    return this.rootNode;
-    // build the html and attributes needed to build the login form with javascript
-    // this.defaultFields.forEach(el => form.appendChild(el))
-  }
+    return this.rootNode
+  };
 
   // arg must be valid html element attr for given node
   addElAttrs = (node, attrs) => {
@@ -48,8 +47,10 @@ class LoginForm {
     }
   };
 
-
-}; // END LoginForm Class
+  render = (node) => {
+    node.appendChild(this.rootNode)
+  };
+}; // END LoginForm
 
 const labelFor = (node) => {
   let labelTag = document.createElement('LABEL');
@@ -57,17 +58,9 @@ const labelFor = (node) => {
   node.insertAdjacentElement('beforebegin', labelTag);
 }
 
-const render = (appendTo, pbNode) => {
-    //function to render form in to html, and render the login form html
-    appendTo.appendChild(pbNode)
-  }
-  // this should be a static class that builds the forms needed
-
-
-
 class SignUpForm {
     // Add only specific information to use for signup for a user.
   
     // many methods should be inherited from the LoginForm class created to render the signup form, with one additional field for password confirmation
     //
-}
+};
